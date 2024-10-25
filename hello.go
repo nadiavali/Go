35451d3267b7626f79889a3226752d7783acee76
IT <NadiaIT@Mac.fritz.box>
2024-10-25 10:56:03 +0200

    go is about lots of fun, and func

--- hello.go
+++ hello.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("helo")
+	fmt.Println(time.Now().UTC().Date())
+	fmt.Println(rand.Intn(3))
+	fmt.Println(math.Pi)
+	print()  // Only to realise how to call a function in go
 
+}
+
+func add(x int,y int)int{
+	return x + y
+}
+
+func print() {
+	fmt.Println(add(42, 78))
 }
